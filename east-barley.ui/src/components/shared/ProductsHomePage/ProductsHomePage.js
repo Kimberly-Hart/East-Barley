942917eb98fc6d942f44e43d642da42fc7447c32
Jamie Phillips <phillipsja97@gmail.com>
2020-06-16 23:26:16 -0500

    added some styling to products and added community section

--- east-barley.ui/src/components/shared/ProductsHomePage/ProductsHomePage.js
+++ east-barley.ui/src/components/shared/ProductsHomePage/ProductsHomePage.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Card, Image, Grid } from 'semantic-ui-react';
+import {
+  Card,
+  Image,
+  Grid,
+  Button,
+} from 'semantic-ui-react';
 import './ProductsHomePage.scss';
 
 class ProductsHome extends React.Component {
@@ -11,118 +16,66 @@ class ProductsHome extends React.Component {
   render() {
     return (
       <div className="productsHome">
-        <div className="productsBanner"></div>
         { (this.state.verified)
-          ? <div className="productsContents">
+          ? <React.Fragment>
+            <div className="productsBanner"></div>
+             <div className="productsContents">
               <div className="productsFinal">
+                <h1 className="homepageProductTitle">Our Products</h1>
                 <div className="productsCards">
-
                 <Grid>
-                    <Grid.Row relaxed columns={3}>
+                    <Grid.Row relaxed columns={4}>
                       <Grid.Column>
-                          <Card className="beerCard">
-                            <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                            <Card.Content>
-                              <Card.Header>We are a Brewery</Card.Header>
-                              <Card.Description>
-                                  We brew all of our own beer.
-                              </Card.Description>
-                            </Card.Content>
+                          <Card className="homeProductCard">
+                            <Image className="cardPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
+                            <Button basic color='blue'>Our Beers Selection</Button>
                           </Card>
                       </Grid.Column>
                       <Grid.Column>
-                        <Card className="whiskeyCard">
-                          <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                          <Card.Content>
-                            <Card.Header>We are a Distillery</Card.Header>
-                            <Card.Description>
-                                We distill all of our own spirits.
-                            </Card.Description>
-                          </Card.Content>
+                        <Card className="homeProductCard">
+                          <Image className="cardPhoto" src='https://www.wine-searcher.com/images/grape/whisky-----single-grain-1747-1-1.jpg' />
+                          <Button basic color='blue'>Our Whiskeys Selection</Button>
                         </Card>
                       </Grid.Column>
                       <Grid.Column>
-                        <Card className="booksCard">
-                          <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                          <Card.Content>
-                            <Card.Header>We are avid readers</Card.Header>
-                            <Card.Description>
-                                Come on in and have a pint and read a local author's book.
-                            </Card.Description>
-                          </Card.Content>
-                        </Card>
-                      </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row relaxed columns={3}>
-                      <Grid.Column>
-                      <Card className="booksCard">
-                          <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                          <Card.Content>
-                            <Card.Header>We are avid readers</Card.Header>
-                            <Card.Description>
-                                Come on in and have a pint and read a local author's book.
-                            </Card.Description>
-                          </Card.Content>
+                        <Card className="homeProductCard">
+                          <Image className="cardPhoto" src='https://images.ctfassets.net/cnu0m8re1exe/4KwrJVfCGeyOKwm8PS2tjI/30026753d97e3b41a50560063126ded8/shutterstock_135114548.jpg?w=650&h=433&fit=fill' />
+                          <Button basic color='blue'>Our Books Selection</Button>
                         </Card>
                       </Grid.Column>
                       <Grid.Column>
-                      <Card className="booksCard">
-                          <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                          <Card.Content>
-                            <Card.Header>We are avid readers</Card.Header>
-                            <Card.Description>
-                                Come on in and have a pint and read a local author's book.
-                            </Card.Description>
-                          </Card.Content>
-                        </Card>
+                          <Card className="homeProductCard">
+                            <Image className="cardPhoto" src='https://i.pinimg.com/originals/48/4a/54/484a54c56e5f87817c70b04f887cf8f9.jpg' />
+                            <Button basic color='blue'>Our Barley Milk Selection</Button>
+                          </Card>
                       </Grid.Column>
                     </Grid.Row>
                   </Grid>
                   </div>
                 </div>
             </div>
-          : <div className="productsContents">
+            </React.Fragment>
+          : <React.Fragment>
+             <div className="productsBanner"></div>
+              <div className="productsContents">
                 <Grid>
-                    <Grid.Row columns={3}>
-                      <Grid.Column>
-                          {/* <Card className="beerCard">
-                            <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                            <Card.Content>
-                              <Card.Header>We are a Brewery</Card.Header>
-                              <Card.Description>
-                                  We brew all of our own beer.
-                              </Card.Description>
-                            </Card.Content>
-                          </Card> */}
-                          <Image className="beerPhoto" src='https://react.semantic-ui.com/images/wireframe/image.png' />
+                    <Grid.Row columns={2}>
+                    <Grid.Column>
+                        <Card className="homeProductCard">
+                          <Image className="cardPhoto" src='https://images.ctfassets.net/cnu0m8re1exe/4KwrJVfCGeyOKwm8PS2tjI/30026753d97e3b41a50560063126ded8/shutterstock_135114548.jpg?w=650&h=433&fit=fill' />
+                          <Button basic color='blue'>Our Books Selection</Button>
+                        </Card>
                       </Grid.Column>
                       <Grid.Column>
-                        {/* <Card className="whiskeyCard">
-                          <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                          <Card.Content>
-                            <Card.Header>We are a Distillery</Card.Header>
-                            <Card.Description>
-                                We distill all of our own spirits.
-                            </Card.Description>
-                          </Card.Content>
-                        </Card> */}
-                        <Image className="beerPhoto" src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                      </Grid.Column>
-                      <Grid.Column>
-                        {/* <Card className="booksCard">
-                          <Image className="beerPhoto" src='https://modistbrewing.com/wp-content/uploads/2017/09/Image-uploaded-from-iOS-3.jpg' />
-                          <Card.Content>
-                            <Card.Header>We are avid readers</Card.Header>
-                            <Card.Description>
-                                Come on in and have a pint and read a local author's book.
-                            </Card.Description>
-                          </Card.Content>
-                        </Card> */}
-                        <Image className="beerPhoto" src='https://react.semantic-ui.com/images/wireframe/image.png' />
+                          <Card className="homeProductCard">
+                            <Image className="cardPhoto" src='https://i.pinimg.com/originals/48/4a/54/484a54c56e5f87817c70b04f887cf8f9.jpg' />
+                            <Button basic color='blue'>Our Barley Milk Selection</Button>
+                          </Card>
                       </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </div>
+          </React.Fragment>
         }
       </div>
     );
