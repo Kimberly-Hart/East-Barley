c188f2a900677187272b62c4d71f1c014a76b340
Ashley Claiborne <languageisaliquid@gmail.com>
2020-06-16 21:45:07 -0500

    routing works

---
+++ east-barley.ui/src/components/pages/AllBooks/AllBooks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AllBooks.scss';
+
+class AllBooks extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Books</h1>
+      </div>
+    );
+  }
+}
+
+export default AllBooks;
---
+++ east-barley.ui/src/components/pages/Profile/Profile.js
@@ -0,0 +1,14 @@
+import './Profile.scss';
+import React from 'react';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile</h1>
+      </div>
+    );
+  }
+}
+
+export default Profile;
