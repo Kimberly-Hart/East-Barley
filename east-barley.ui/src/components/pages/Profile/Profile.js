9d19b48c5f052e7eba6550e045fffef80191722e
Jamie Phillips <phillipsja97@gmail.com>
2020-06-20 15:06:07 -0500

    added account title

--- east-barley.ui/src/components/pages/Profile/Profile.js
+++ east-barley.ui/src/components/pages/Profile/Profile.js
@@ -1,9 +1,11 @@
-import './Profile.scss';
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { Header, Icon } from 'semantic-ui-react';
 import userData from '../../../helpers/data/userData';
 import authData from '../../../helpers/data/authData';
+import './Profile.scss';
+
 
 class Profile extends React.Component {
   state = {
@@ -23,8 +25,15 @@ class Profile extends React.Component {
   render() {
     const { user } = this.state;
     return (
-      <div>
-        <h1>{user.firebaseUID}</h1>
+      <div className="profilePage">
+        <div className="accountTitle">
+           <Header as='h2'>
+              <Icon name='settings' />
+              <Header.Content>
+                Account Information
+              </Header.Content>
+            </Header>
+            </div>
       </div>
     );
   }
