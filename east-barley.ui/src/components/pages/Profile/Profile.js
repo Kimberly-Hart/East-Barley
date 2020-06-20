7d89aa3639c3a1b8eef2548359d528d988c04ab3
Jamie Phillips <phillipsja97@gmail.com>
2020-06-20 14:00:51 -0500

    added get user by firebaseUID function

--- east-barley.ui/src/components/pages/Profile/Profile.js
+++ east-barley.ui/src/components/pages/Profile/Profile.js
@@ -1,11 +1,30 @@
 import './Profile.scss';
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import userData from '../../../helpers/data/userData';
+import authData from '../../../helpers/data/authData';
 
 class Profile extends React.Component {
+  state = {
+    user: {},
+  }
+
+  // getUid = () => firebase.auth().currentUser.uid;
+
+  componentDidMount() {
+    userData.getUserByUID(firebase.auth().currentUser.uid)
+      .then((user) => {
+        this.setState({ user });
+      })
+      .catch((errorFromProfile) => console.error(errorFromProfile));
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <div>
-        <h1>Profile</h1>
+        <h1>{user.firebaseUID}</h1>
       </div>
     );
   }
