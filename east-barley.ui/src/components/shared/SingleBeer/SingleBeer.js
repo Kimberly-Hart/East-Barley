e8822b8bbfb8e4e7fc64ca5f9c04a5ec94b53100
Jamie Phillips <phillipsja97@gmail.com>
2020-06-24 20:11:38 -0500

    product modal works and is styled

--- east-barley.ui/src/components/shared/SingleBeer/SingleBeer.js
+++ east-barley.ui/src/components/shared/SingleBeer/SingleBeer.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import beersShape from '../../../helpers/propz/productsShape';
+import ProductModal from '../ProductModal/ProductModal';
 import './SingleBeer.scss';
 
 class SingleBeer extends Component {
-    static propTypes = {
-      beer: beersShape.beersShape,
-    }
+  state = {
+    modalOpen: false,
+  }
 
-    render() {
-      const { beer } = this.props;
-      return (
-        <div className="SingleBeer">
-            <Card className="beerCard"
-              image={beer.imageUrl}
-              header={beer.title}
-              meta={beer.category}
-              description={beer.description}
-              extra={this.button}
-            />
-                {/* <li>Price: {beer.price}</li>
-                <li>Quantity: {beer.quantity}</li> */}
-        </div>
-      );
-    }
+  static propTypes = {
+    beer: beersShape.beersShape,
+  }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false })
+
+  render() {
+    const { beer } = this.props;
+    const { modalOpen } = this.state;
+    return (
+      <div className="SingleBeer">
+          <Card className="beerCard"
+            image={beer.imageUrl}
+            header={beer.title}
+            meta={beer.category}
+            description={beer.description}
+            extra={this.button}
+            onClick={this.handleOpen}
+          />
+            <Button onClick={this.handleOpen}>Show Modal</Button>
+              {/* <li>Price: {beer.price}</li>
+              <li>Quantity: {beer.quantity}</li> */}
+            <ProductModal modalOpen={modalOpen} product={beer} handleClose={this.handleClose} handleOpen={this.handleOpen} />
+      </div>
+    );
+  }
 }
 
 export default SingleBeer;
